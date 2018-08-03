7b9c9b3f44ae732eae51a0211385ee3490592b01
manuel <manuel.garrido.pena@gmail.com>
2018-08-03 12:31:34 +0200

    actualizado datos nombres bebes

--- Secciones/Seccion2.Intro_a_python/ejercicios/ejercicios_avanzados/nombres_bebes/nombres_bebes_solucion.py
+++ Secciones/Seccion2.Intro_a_python/ejercicios/ejercicios_avanzados/nombres_bebes/nombres_bebes_solucion.py
@@ -16,16 +16,16 @@ En concreto, esta aplicación hará lo siguiente.
 """
 
 import argparse
-from collections import defaultdict
 
 
 def parsear_argumentos_avanzado():
     parser = argparse.ArgumentParser(
-        description="Descripción del script"
+        description="""Este script calcula el numero de años desde 2002 que el nombre indicado fue de
+                    los más populares en España"""
     )
 
     parser.add_argument("nombre", help="""
-                        Nombre que queremos analizar
+                            Nombre que queremos analizar
                         """)
 
     argumentos = parser.parse_args()
@@ -38,29 +38,29 @@ def cargar_archivo(year):
     nombres_masculinos = []
     nombres_femeninos = []
     for linea_nombres in lineas:
-        nombres = linea_nombres.split(",")
+        nombres = linea_nombres.strip().split(",")
+        assert len(nombres) == 2
         nombres_masculinos.append(nombres[0])
-        nombres_femeninos.append(nombres[2])
+        nombres_femeninos.append(nombres[1])
     return nombres_masculinos + nombres_femeninos
 
 
-def calcular_popularidad_por_nombre(nombre):
-    popularidad_nombre_dict = defaultdict(lambda: 0)
-
+def calcular_popularidad_por_nombre(nombre_busqueda):
+    years_nombre = 0
     for year in ["02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16"]:
         nombres_year = cargar_archivo(year)
-        for nombre in nombres_year:
-            popularidad_nombre_dict[nombre] += 1
-    return popularidad_nombre_dict
-
+        if nombre_busqueda in nombres_year:
+            years_nombre += 1
+    return years_nombre
 
 def main(argumentos):
     """
     Aquí ponemos la funcionalidad principal de nuestro script
     """
     nombre = argumentos.nombre.upper()
-    popularidad_nombre_dict = calcular_popularidad_por_nombre(nombre)
-    print("El nombre {} está entre los más populares en {} años".format(nombre, popularidad_nombre_dict[nombre]))
+    years_nombre = calcular_popularidad_por_nombre(nombre)
+    print("El nombre {} está entre los más populares en {} años".format(nombre, years_nombre))
+
 
 
 if __name__ == "__main__":
